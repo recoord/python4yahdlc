66f897bb24ce16c6dd114120148a9f30c4805146
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2019-05-15 22:35:29 +0100

    Fix most linting errors in unit tests

--- test/test.py
+++ test/test.py
@@ -1,8 +1,23 @@
+"""
+Unit tests written with the 'unittest' module.
+"""
+
 import unittest
-from yahdlc import *
+from yahdlc import (get_data, frame_data,
+                    FRAME_DATA, FRAME_ACK, FRAME_NACK,
+                    MessageError, FCSError)
+
 
 class TestYahdlc(unittest.TestCase):
+    """
+    Unit tests for the yahdlc module.
+    """
+
     def test_frame_data_invalid_inputs(self):
+        """
+        Call 'frame_data' with invalid inputs.
+        """
+
         with self.assertRaises(TypeError):
             frame_data(2)
         with self.assertRaises(TypeError):
@@ -15,6 +30,10 @@ class TestYahdlc(unittest.TestCase):
             frame_data('test', FRAME_DATA, 8)
 
     def test_get_data_invalid_inputs(self):
+        """
+        Call 'get_data' with invalid inputs.
+        """
+
         with self.assertRaises(TypeError):
             get_data(12)
         with self.assertRaises(MessageError):
@@ -23,6 +42,11 @@ class TestYahdlc(unittest.TestCase):
             get_data('test')
 
     def test_get_data_corrupted_frame(self):
+        """
+        Call 'get_data' with a corrupted frame
+        as input.
+        """
+
         data = bytearray(frame_data('test', FRAME_DATA, 0))
         data[7] ^= 0x01
         data = bytes(data)
@@ -46,6 +70,11 @@ class TestYahdlc(unittest.TestCase):
                 raise
 
     def test_encode_and_decode_frame(self):
+        """
+        Encode a frame using 'frame_data', and then
+        decode the same frame using 'get_data'.
+        """
+
         frame = frame_data('test')
         data, ftype, seq_no = get_data(frame)
 
@@ -53,7 +82,12 @@ class TestYahdlc(unittest.TestCase):
         self.assertEqual(FRAME_DATA, ftype)
         self.assertEqual(0, seq_no)
 
-    def test_decode_frame_with_1B_buffer(self):
+    def test_decode_frame_byte_per_byte(self):
+        """
+        Decode a frame byte per byte using
+        'get_data'.
+        """
+
         frame = frame_data('test')
 
         for c in frame:
@@ -65,6 +99,7 @@ class TestYahdlc(unittest.TestCase):
         self.assertEqual(b'test', data)
         self.assertEqual(FRAME_DATA, ftype)
         self.assertEqual(0, seq_no)
+
 
 if __name__ == '__main__':
     unittest.main()
